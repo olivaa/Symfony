b0f3b31a15035fa6701df2ea93857af9a216ec34
olivaa <alolrod@epsa.upv.es>
2018-02-14 14:32:19 +0100

    Tema 6- Services, Request&Response, Session

--- src/Controller/DeportesController.php
+++ src/Controller/DeportesController.php
@@ -10,17 +10,53 @@ namespace App\Controller;
 
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+
 class DeportesController extends Controller
 
 {
     /**
-     * @Route("/deportes/")
+     * @Route("/deportes", name="inicio" )
      */
     public function inicio()
     {
         return new Response('Mi página de deportes!');
+    }
+
+    /**
+     * @Route("/deportes/usuario", name="usuario" )
+     */
+    public function serionUsuario(Request $request)
+    {
+        $usuarios=["alejandro","manuel","adrian"];
+        $usuario_get=$request->query->get('nombre');
+
+
+        if(in_array($usuario_get,$usuarios)) {
+            $session = new Session();
+            $session->start();
+            $session->set('nombre', $usuario_get);
+
+            return $this->redirectToRoute('usuario_session',array('nombre'=>$usuario_get));
+        }
+        return new Response(sprintf('Sesion invalida para usuario con nombre: %s'
+            , $usuario_get));
+    }
+
+    /**
+     * @Route("/deportes/usuario/{nombre}", name="usuario_session" )
+     */
+    public function paginaUsuario()
+    {
+        $session=new Session();
+        $usuario=$session->get('nombre');
+        return new Response(sprintf('Sesion iniciada con el atributo nombre: %s'
+            , $usuario
+        ));
     }
 
     /**
@@ -32,6 +68,7 @@ class DeportesController extends Controller
     {
         //Simulamos una baser de datos de deportes
         $deportes=["futbol", "tenis","rugby"];
+
 
         //Si el deporte que buscamos no se encuentra lanzamos la
         //excepcion 404 deporte no encontrado
@@ -95,11 +132,20 @@ class DeportesController extends Controller
      */
     public function rutaAvanzada($_idioma,$fecha, $seccion, $equipo, $slug)
     {
+        //Simulamos una base de datos de equipos o personas
+        $deportes=["valencia", "barcelona","federer", "rafa-nadal"];
+
+        //Si el equipo o persona que buscamos no se encuentra redirigimos
+        //al usuario a la pagina de inicio
+        if(!in_array($equipo,$deportes)){
+           return $this->redirectToRoute('inicio');
+        }
         return new Response(sprintf(
             'Mi noticia en idioma=%s,
              fehca=%s,deporte=%s,equipo=%s, noticia=%s ',
             $_idioma, $fecha, $seccion, $equipo, $slug));
     }
+
 
 
 }
